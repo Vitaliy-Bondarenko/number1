49729b5bb4606d98bca8ad6c0bd4fa2a1eb5761b
Vitaliy Bondarenko <vitaliy@gmail.com>
2014-08-29 11:02:59 +0300

    Show\edit\index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,18 @@ def registration_user
   #RegistrationMailer.user_active(@user).deliver
 end
 
+def edit
+  @user = User.find(params[:id])
+end
+
+def show
+  @user = User.find(params[:id])
+end
+
+def index
+  @user = User.all
+end
+
 def create
 	#binding.pry
   @user = User.new(user_param)
